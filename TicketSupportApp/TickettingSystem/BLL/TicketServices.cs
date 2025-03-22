cd45e494648057fcb7394d5b5b36b8ba91e33289
Trintin Affleck <t.affleck101@hotmail.com>
2025-03-22 11:08:35 -0600

    Added Ticketting Backend

---
+++ TicketSupportApp/TickettingSystem/BLL/TicketServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSystem.DAL;
+using TicketSystem.Models;
+
+namespace TicketSystem.BLL
+{
+    public class TicketServices
+    {
+        private readonly TicketSupportContext _ticketSupportContext;
+
+        internal TicketServices(TicketSupportContext context)
+        {
+            _ticketSupportContext = context;
+        }
+
+        public List<Ticket> GetAllTickets()
+        {
+            return _ticketSupportContext.Tickets.Select(a => a).ToList();
+        }
+    }
+}
---
+++ TicketSupportApp/TickettingSystem/TicketExtentions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TicketSystem.BLL;
+using TicketSystem.DAL;
+
+namespace TickettingSystem
+{
+    public static class TicketExtentions
+    {
+        public static void AddBackendDependencies(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            services.AddDbContext<TicketSupportContext>(options);
+            services.AddTransient<TicketServices>(ServiceProvider =>
+            {
+                var context = ServiceProvider.GetService<TicketSupportContext>() ?? throw new Exception("Ticket Service was null");
+                return new TicketServices(context);
+            });
+        }
+    }
+}
