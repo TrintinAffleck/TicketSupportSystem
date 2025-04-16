a5697791e0f60c742f1571cc0faa5478465a2dc8
Trintin Affleck <t.affleck101@hotmail.com>
2025-04-16 13:02:35 -0600

    error handling and del unused function

--- TicketSupportApp/TicketSupportWebApp/Components/Pages/Tickets.razor.cs
+++ TicketSupportApp/TicketSupportWebApp/Components/Pages/Tickets.razor.cs
@@ -4,42 +4,65 @@ using MudBlazor;
 using TicketSystem.BLL;
 using TicketSystem.Models;
 using TicketSystem.ViewModels;
-
+using Utilities;
 namespace TicketSupportWebApp.Components.Pages
 {
 	public partial class Tickets : ComponentBase
 	{
 		[Inject]
-		TicketServices ticketServices { get; set; }
+		TicketServices? TicketServices { get; set; }
 
 		[Inject]
-		NavigationManager navigationManager { get; set; }
+		NavigationManager? NavigationManager { get; set; }
 
 		List<TicketView> tickets = [];
 		List<TicketCategory> categories = [];
 		List<TicketStatus> statuses = new List<TicketStatus>();
+		List<string> errors = new List<string>();
 
 		TicketView test;
 
 		protected override async Task OnInitializedAsync()
 		{
-			tickets = ticketServices.GetAllTickets();
-			categories = ticketServices.GetTicketCategories();
-			statuses = ticketServices.GetTicketStatuses();
+			if (TicketServices == null)
+			{
+				throw new ArgumentNullException("Ticket Services is not available.");
+			}
+			if (NavigationManager == null)
+			{
+				throw new ArgumentNullException("Navigation Manager is not available.");
+			}
+			try
+			{
+				tickets = TicketServices.GetAllTickets();
+				categories = TicketServices.GetTicketCategories();
+				statuses = TicketServices.GetTicketStatuses();
+			}
+			catch (ArgumentNullException ex)
+			{
+				errors.Add(Utils.GetInnerException(ex).Message);
+			}
+			
+
 			await base.OnInitializedAsync();
 		}
 
 		private void Save(TicketView ticket)
 		{
-			
-			ticketServices.SaveTicket(ticket);
+			try
+			{
+				TicketServices.SaveTicket(ticket);
+			}
+			catch (ArgumentException ex)
+			{
+				errors.Add(Utils.GetInnerException(ex).Message);
+			}
+
 		}
 
-		private void OnCategoryChange(TicketView ticketView)
-		{
-			test = ticketView;
-			Console.WriteLine(ticketView.Category);
-		}
+		
+
+		
 
 		
 	}
