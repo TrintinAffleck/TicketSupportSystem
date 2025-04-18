1376e8806e3ddfbbeeca5bf1be436cb86626c078
Trintin Affleck <t.affleck101@hotmail.com>
2025-04-18 13:01:59 -0600

    Validation on TicketView

--- TicketSupportApp/TickettingSystem/ViewModels/TicketView.cs
+++ TicketSupportApp/TickettingSystem/ViewModels/TicketView.cs
@@ -12,38 +12,52 @@ namespace TicketSystem.ViewModels
 {
 	public class TicketView
 	{
+		[Key]
 		public int TicketID { get; set; }
-
+		
+		[Required]
+		[StringLength(200)]
+		[Unicode(false)]
 		public required string Subject { get; set; }
 
+		[Required]
+		[Column(TypeName = "text")]
 		public required string Description { get; set; }
 
+		[Required]
+		[StringLength(450)]
 		public required string CustomerID { get; set; }
 
+		[Required]
 		public required AspNetUser Customer { get; set; }
 
+		[StringLength(450)]
 		public string? AssignedAgentID { get; set; }
 
 		public AspNetUser? AssignedAgent { get; set; }
 
+		[Required]
 		public int StatusID { get; set; }
 
+		[Required]
 		public required TicketStatus Status { get; set; }
 
+		[Required]
 		public int PriorityID { get; set; }
 
+		[Required]
 		public required TicketPriority Priority { get; set; }
-
+		[Required]
 		public int CategoryID { get; set; }
-
+		[Required]
 		public required TicketCategory Category { get; set; }
-
+		[Column(TypeName = "datetime")]
 		public DateTime CreatedAt { get; set; }
-
+		[Column(TypeName = "datetime")]
 		public DateTime UpdatedAt { get; set; }
-
+		[Column(TypeName = "datetime")]
 		public DateTime? ResolvedAt { get; set; }
-
+		[Column(TypeName = "datetime")]
 		public DateTime? SLA_DueDate { get; set; }
 
 		public List<TicketComment>? CommentList { get; set; }
