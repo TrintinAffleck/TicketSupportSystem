081c69a7b5060a947c12cc986c93cdde09d7dbd9
Trintin Affleck <t.affleck101@hotmail.com>
2025-04-18 19:44:22 -0600

    Data Annotations

--- TicketSupportApp/TickettingSystem/ViewModels/TicketView.cs
+++ TicketSupportApp/TickettingSystem/ViewModels/TicketView.cs
@@ -15,20 +15,20 @@ namespace TicketSystem.ViewModels
 		[Key]
 		public int TicketID { get; set; }
 		
-		[Required]
+		[Required(ErrorMessage ="Subject is Required!")]
 		[StringLength(200)]
 		[Unicode(false)]
 		public required string Subject { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Description is Required!")]
 		[Column(TypeName = "text")]
 		public required string Description { get; set; }
 
-		[Required]
+		[Required(ErrorMessage ="Customer ID is Required!")]
 		[StringLength(450)]
 		public required string CustomerID { get; set; }
 
-		[Required]
+		[Required(ErrorMessage ="Customer is Required!")]
 		public required AspNetUser Customer { get; set; }
 
 		[StringLength(450)]
@@ -36,21 +36,24 @@ namespace TicketSystem.ViewModels
 
 		public AspNetUser? AssignedAgent { get; set; }
 
-		[Required]
+		[Required(ErrorMessage ="Status ID is Required!")]
 		public int StatusID { get; set; }
 
-		[Required]
+		[Required(ErrorMessage ="Status is Required!")]
 		public required TicketStatus Status { get; set; }
 
-		[Required]
+		[Required(ErrorMessage ="Priority ID is Required!")]
 		public int PriorityID { get; set; }
 
-		[Required]
+		[Required(ErrorMessage ="Priority is Required!")]
 		public required TicketPriority Priority { get; set; }
-		[Required]
+
+		[Required(ErrorMessage ="Category ID is Required!")]
 		public int CategoryID { get; set; }
-		[Required]
+
+		[Required(ErrorMessage ="Category is Required!")]
 		public required TicketCategory Category { get; set; }
+
 		[Column(TypeName = "datetime")]
 		public DateTime CreatedAt { get; set; }
 		[Column(TypeName = "datetime")]
