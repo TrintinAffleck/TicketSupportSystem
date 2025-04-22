ad3921885dfaf2a184364fdf3478c7bad32d00bc
Trintin Affleck <t.affleck101@hotmail.com>
2025-04-22 10:46:45 -0600

    Server Side Data Retreival, Filtering and Sorting.

--- TicketSupportApp/TicketSupportWebApp/Components/Pages/Tickets.razor.cs
+++ TicketSupportApp/TicketSupportWebApp/Components/Pages/Tickets.razor.cs
@@ -5,6 +5,7 @@ using TicketSystem.BLL;
 using TicketSystem.Models;
 using TicketSystem.ViewModels;
 using Utilities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace TicketSupportWebApp.Components.Pages
 {
 	public partial class Tickets : ComponentBase
@@ -15,14 +16,14 @@ namespace TicketSupportWebApp.Components.Pages
 		[Inject]
 		NavigationManager? NavigationManager { get; set; }
 
-		List<TicketView>? tickets = null;
+		IEnumerable<TicketView>? tickets = null;
 		List<TicketCategory> categories = [];
 		List<TicketStatus> statuses = new List<TicketStatus>();
 		List<string> errors = new List<string>();
-		private TicketView test;
+		private MudDataGrid<TicketView> dataGrid;
 		private MudForm ticketForm;
 		private bool isTicketsLoading = true;
-
+		private string? searchString = null;
 		protected override async Task OnInitializedAsync()
 		{
 			if (TicketServices == null)
@@ -33,6 +34,16 @@ namespace TicketSupportWebApp.Components.Pages
 			{
 				throw new ArgumentNullException("Navigation Manager is not available.");
 			}
+
+			await base.OnInitializedAsync();
+		}
+
+		private async Task<GridData<TicketView>> LoadTickets(GridState<TicketView> state)
+		{
+			if (state == null)
+			{
+				throw new ArgumentNullException("state is null");
+			}
 			try
 			{
 				tickets = await TicketServices.GetAllTickets();
@@ -42,26 +53,77 @@ namespace TicketSupportWebApp.Components.Pages
 				}
 				categories = TicketServices.GetTicketCategories();
 				statuses = TicketServices.GetTicketStatuses();
+
+				tickets = tickets.Where(ticket =>
+				{
+					if (string.IsNullOrWhiteSpace(searchString))
+						return true;
+					if (ticket.Description.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+						return true;
+					if (ticket.Subject.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+						return true;
+					if ($"{ticket.TicketID} {ticket.Category.CategoryName} {ticket.Status.StatusName}".Contains(searchString))
+						return true;
+					return false;
+				}).ToArray();
+
+				int totalItems = tickets.Count();
+
+				SortDefinition<TicketView> sortDefinition = state.SortDefinitions.FirstOrDefault();
+				if (sortDefinition != null)
+				{
+					switch (sortDefinition.SortBy)
+					{
+						case nameof(TicketView.TicketID):
+							tickets = tickets.OrderByDirection(
+								sortDefinition.Descending ? SortDirection.Descending : SortDirection.Ascending,
+								o => o.TicketID
+							);
+							break;
+						case nameof(TicketView.Subject):
+							tickets = tickets.OrderByDirection(
+								sortDefinition.Descending ? SortDirection.Descending : SortDirection.Ascending,
+								o => o.Subject
+							);
+							break;
+						case nameof(TicketView.Description):
+							tickets = tickets.OrderByDirection(
+								sortDefinition.Descending ? SortDirection.Descending : SortDirection.Ascending,
+								o => o.Description
+							);
+							break;
+						case nameof(TicketView.Category.CategoryName):
+							tickets = tickets.OrderByDirection(
+								sortDefinition.Descending ? SortDirection.Descending : SortDirection.Ascending,
+								o => o.Category.CategoryName
+							);
+							break;
+						case nameof(TicketView.Status.StatusName):
+							tickets = tickets.OrderByDirection(
+								sortDefinition.Descending ? SortDirection.Descending : SortDirection.Ascending,
+								o => o.Status.StatusName
+							);
+							break;
+					}
+				}
+				var pagedData = tickets.Skip(state.Page * state.PageSize).Take(state.PageSize).ToArray();
+				return new GridData<TicketView>
+				{
+					TotalItems = totalItems,
+					Items = pagedData,
+				};
 			}
 			catch (ArgumentNullException ex)
 			{
 				errors.Add(Utils.GetInnerException(ex).Message);
+				return null;
 			}
-			
-
-			await base.OnInitializedAsync();
 		}
-
-		private static string Validate(ref string stringToValidate)
+		private Task OnSearch(string text)
 		{
-			if (stringToValidate == null || stringToValidate == "")
-			{
-				return "Subject cannot be empty!";
-			}
-			
-			return null;
+			searchString = text;
+			return dataGrid.ReloadServerData();
 		}
-
 		private async void Save(TicketView ticket)
 		{
 			try
@@ -72,7 +134,7 @@ namespace TicketSupportWebApp.Components.Pages
 			{
 				errors.Add(Utils.GetInnerException(ex).Message);
 			}
-			
+
 		}
 
 		private void PrintSomething()
