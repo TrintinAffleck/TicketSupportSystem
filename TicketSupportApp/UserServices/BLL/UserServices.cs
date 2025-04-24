3f20e3ebbc4d0193403b552e5f12fdf26ce0ab29
Trintin Affleck <t.affleck101@hotmail.com>
2025-04-23 19:15:23 -0600

    Creating a service for Users

---
+++ TicketSupportApp/UserServices/BLL/UserServices.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserServices.DAL;
+using UserServices.Models;
+
+namespace UserServices.BLL
+{
+	public class UserServices
+	{
+		private readonly UserServicesContext _userServicesContext;
+
+		internal UserServices(UserServicesContext userServicesContext)
+		{
+			_userServicesContext = userServicesContext;
+		}
+
+		public AspNetUser? GetUserById(string id)
+		{
+			return _userServicesContext.AspNetUsers.Select(s => s).Where(t => t.Id == id).FirstOrDefault();
+		}
+	}
+}
